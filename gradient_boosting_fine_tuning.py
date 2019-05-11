07add5e48fdf9884fc5cbace90bcb708670aac96
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-11 15:04:10 -0700

    fine tune the xgboost with the no. of trees

---
+++ gradient_boosting_fine_tuning.py
@@ -0,0 +1,45 @@
+
+import xgboost as xgb
+"""
+Tuning the number of boosting rounds:
+
+Let's start with parameter tuning by seeing how the number
+of boosting rounds (number of trees you build) impacts the out-of-sample performance of your XGBoost model.
+
+working with the Ames housing dataset.
+
+"""
+
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary for each tree: params
+params = {"objective": "reg:linear", "max_depth": 3}
+
+# Create list of number of boosting rounds
+num_rounds = [5, 10, 15]
+
+# Empty list to store final round rmse per XGBoost model
+final_rmse_per_round = []
+
+# Iterate over num_rounds and build one model per num_boost_round parameter
+for curr_num_rounds in num_rounds:
+    # Perform cross-validation: cv_results
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=3, num_boost_round=curr_num_rounds, metrics="rmse",
+                        as_pandas=True, seed=123)
+
+    # Append final round RMSE
+    final_rmse_per_round.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+num_rounds_rmses = list(zip(num_rounds, final_rmse_per_round))
+print(pd.DataFrame(num_rounds_rmses, columns=["num_boosting_rounds", "rmse"]))
+
+
+"""
+   num_boosting_rounds          rmse
+0                    5  50903.299479
+1                   10  34774.194010
+2                   15  32895.098958
+"""
