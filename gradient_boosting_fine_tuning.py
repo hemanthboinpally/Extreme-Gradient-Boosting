fcda94a3938be0d7df23064eb0e90a259aa53b49
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-11 15:50:17 -0700

    Tune max_depth

--- gradient_boosting_fine_tuning.py
+++ gradient_boosting_fine_tuning.py
@@ -193,3 +193,35 @@ max_depth, which is the parameter that dictates the maximum depth that each tree
 Smaller values will lead to shallower trees, and larger values to deeper trees.
 
 """
+
+# Create your housing DMatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary
+params = {"objective": "reg:linear"}
+
+# Create list of max_depth values
+max_depths = [2, 5, 10, 20]
+best_rmse = []
+
+# Systematically vary the max_depth
+for curr_val in max_depths:
+    params["max_depth"] = curr_val
+
+    # Perform cross-validation
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, folds=2, num_boost_round=10, early_stopping_rounds=5,
+                        metrics="rmse", seed=123)
+
+    # Append the final round rmse to best_rmse
+    best_rmse.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+print(pd.DataFrame(list(zip(max_depths, best_rmse)), columns=["max_depth", "best_rmse"]))
+
+"""
+   max_depth     best_rmse
+0          2  36132.386068
+1          5  32000.271484
+2         10  32862.851563
+3         20  32805.277995
+"""
