f96ec42502ce40f69bd262233dc9067d53aec5a6
hemanthboinpally <hemanth.boinpally@gmail.com>
2019-05-12 14:18:11 -0700

    Pipeline

--- gradient_boosting_pipelining.py
+++ gradient_boosting_pipelining.py
@@ -152,3 +152,30 @@ OUTPUT:
 
 
 """
+
+#############
+
+"""
+4) Preprocessing pipeline 
+
+Ames housing data, let's use the much cleaner and more succinct DictVectorizer approach and put it alongside an 
+XGBoostRegressor inside of a scikit-learn pipeline.
+
+"""
+
+# Import necessary modules
+from sklearn.feature_extraction import DictVectorizer
+from  sklearn.pipeline import Pipeline
+
+# Fill LotFrontage missing values with 0
+X.LotFrontage = X.LotFrontage.fillna(0)
+
+# Setup the pipeline steps: steps
+steps = [("ohe_onestep", DictVectorizer(sparse=False)),
+         ("xgb_model", xgb.XGBRegressor())]
+
+# Create the pipeline: xgb_pipeline
+xgb_pipeline = Pipeline(steps)
+
+# Fit the pipeline
+xgb_pipeline.fit(X.to_dict("records"), y)
